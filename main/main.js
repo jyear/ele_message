2244c986a492a55e310c7cadf04df2a24c86d724
yangxu <yx53074@hotmail.com>
2022-03-18 16:17:08 +0800

    feat: 修改

--- main/main.js
+++ main/main.js
@@ -4,7 +4,6 @@ const path = require("path");
 const resources = path.resolve(__dirname, "../resources");
 const sql = require("../sql/index");
 
-// sql.users.insert();
 app.on("ready", () => {
   const win = createWindow();
   win.loadFile(path.resolve(resources, "./index.html"));
